b4d41bdc3a407e960c49e8a3b24037199f498c98
filaudea <marius.filaudeau@etu.u-paris.fr>
2024-11-14 20:00:01 +0100

    fonction run_rect

--- src/approx.ml
+++ src/approx.ml
@@ -27,8 +27,23 @@ let transform_rect (t : transformation) (r : rectangle) : rectangle =
     (* On retourne le plus petit rectangle qui contient chaque point de la liste *)
     rectangle_of_list corners_after_rotation
 
+(* Fonction très similaire a run dans le fichier interp mais avec des rectangles à la place des points *)
 let run_rect (prog : program) (r : rectangle) : rectangle list =
-  failwith "À compléter"
+  let unfolded_prog = unfold_repeat prog in
+  let rec execute_program prog current_rectangle visited_rectangles =
+    match prog with
+    | [] -> List.rev visited_rectangles
+    | Either (first_prog, second_prog) :: _ -> 
+      let random = Random.bool () in 
+      if random then execute_program first_prog r visited_rectangles 
+      else execute_program second_prog r visited_rectangles
+    | Repeat _ :: _ -> failwith "error in unfold_repeat"
+    | Move t :: rest ->
+        (* On calcule le nouveau rectangle avec la fonction précèdente *)
+        let new_rect = transform_rect t current_rectangle in
+        execute_program rest new_rect (new_rect :: visited_rectangles)
+  in
+  execute_program unfolded_prog r [r]
 
 let inclusion (r : rectangle) (t : rectangle) : bool =
   failwith "À compléter"
